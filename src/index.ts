50401de5b926cd809482538f612a09843753cf01
boubad <boubadiarra@gmail.com>
2021-11-15 23:49:14 +0100

    work on index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export * from './lib/async';
 export * from './lib/hash';
 export * from './lib/number';
+//
+export * from './lib/IAttachedDoc';
+export * from './lib/IBaseDoc';
+export * from './lib/IDataOption';
+export * from './lib/IDataStore';
+export * from './lib/IDataUrlCreator';
+export * from './lib/IFetchClient';
+export * from './lib/IHttpOutput';
+export * from './lib/IItemPayload';
+export * from './lib/StatusType';
